0f38c3094f90612dd07408026762bfab2340ff8f
Robert DeLuca <robertd@izea.com>
2015-04-05 17:43:50 -0500

    Using ember watson to update tests

--- tests/unit/helpers/format-markdown-test.js
+++ tests/unit/helpers/format-markdown-test.js
@@ -1,11 +1,12 @@
-import {
-  formatMarkdown
-} from 'markdown-code-highlighting/helpers/format-markdown';
+import { formatMarkdown } from 'markdown-code-highlighting/helpers/format-markdown';
+
+import { module, test } from 'qunit';
 
 module('FormatMarkdownHelper');
 
 // Replace this with your real tests.
-test('it works', function() {
+test('it works', function(assert) {
   var result = formatMarkdown(42);
-  ok(result);
+
+  assert.ok(result);
 });
